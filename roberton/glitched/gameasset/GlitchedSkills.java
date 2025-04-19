3a4e9235814287372daad3fa57d0afb69f95a128
1Robert-on1 <theroberton03@gmail.com>
2025-04-20 01:03:56 +0300

    Update GlitchedSkills.java

--- roberton/glitched/gameasset/GlitchedSkills.java
+++ roberton/glitched/gameasset/GlitchedSkills.java
@@ -1,1 +1,16 @@
+package robert_on.glitched.gameasset;
 
+import java.util.Set;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import yesman.epicfight.skill.dodge.DodgeSkill;
+
+public class EpicFightSkills {
+  	/** Dodging skills **/
+	public static Skill GLITCHCHASER;
+	public static Skill GLITCHSTEP;
+
+  public static void buildSkillEvent(SkillBuildEvent build) {
+		ModRegistryWorker modRegistry = build.createRegistryWorker(EpicFightMod.MODID);
+
+		STEP = modRegistry.build("step", DodgeSkill::new, DodgeSkill.createDodgeBuilder().setAnimations(Animations.BIPED_STEP_FORWARD, Animations.BIPED_STEP_BACKWARD, Animations.BIPED_STEP_LEFT, Animations.BIPED_STEP_RIGHT));
